84be37c95460e6267d335975ec81f6b4bfc51cd7
Priya <piyupriyapatil@gmail.com>
2021-01-17 10:46:47 -0600

    First Commit

---
+++ src/test/java/com/priya/rightcare/CapstoneRightcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priya.rightcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneRightcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
